959fa13ab8e72b8074f68dabd75dab86b98263ab
Megha Prasad Bindiganavale <meghab.prasad@gmail.com>
2019-07-15 17:59:53 -0700

    fix file struc

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
-const router = require("express").Router();
-const apiRoutes = require("./api");
+const express = require('express')
+const router = express.Router()
 
+const apiRoutes = require("./api")
 
+router.use("/api", apiRoutes);
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Gymate',
+    user: req.user,
+  })
+})
 
-//API Routes
-router.use("/api", apiRoutes)
-
-
-
-// // Authentication Routes
-// router.use("/auth", authRoutes);
-
-
-module.exports = router;
+module.exports = router
